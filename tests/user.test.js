bfe135d647c9dddf0b2b36b3712006b5ff395005
Bruno Bigotto <brunobigotto@hotmail.com>
2020-07-23 22:15:34 -0400

    Criação dos testes unitários

---
+++ tests/user.test.js
@@ -0,0 +1,106 @@
+import 'cross-fetch/polyfill'
+import ApolloBoost, { gql } from 'apollo-boost'
+import bcript from 'bcryptjs'
+import prisma from '../src/prisma'
+
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000'
+})
+
+beforeEach(async () => {
+    await prisma.mutation.deleteManyPosts()
+    await prisma.mutation.deleteManyUsers()
+    const user = await prisma.mutation.createUser({
+        data: {
+            name: "teste user",
+            email: "teste@email.com",
+            password: bcript.hashSync('12345678')
+        }
+    })
+    await prisma.mutation.createPost({
+        data: {
+            title: "my post",
+            body: "",
+            published: true,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
+    await prisma.mutation.createPost({
+        data: {
+            title: "my post 2",
+            body: "",
+            published: false,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    })
+})
+
+test('Should create a new user', async () => {
+    const createUser = gql`
+        mutation {
+            createUser (
+                data: {
+                    name: "Bruno",
+                    email: "bruno@bigotto.com",
+                    password: "12345678"
+                }
+            ){
+                token,
+                user {
+                   id 
+                }
+            }
+        }
+    `
+
+    const response = await client.mutate({
+        mutation: createUser
+    })
+
+    const exists = await prisma.exists.User({ id: response.data.createUser.user.id })
+    expect(exists).toBe(true)
+})
+
+test('Should expose public author profiles', async () => {
+    const getUsers = gql`
+        query {
+            users{
+                id
+                name
+                email
+            }
+        }
+    `
+
+    const response = await client.query({ query: getUsers })
+
+    expect(response.data.users.length).toBe(1)
+    expect(response.data.users[0].email).toBe(null)
+    expect(response.data.users[0].name).toBe('teste user')
+})
+
+test('Should expose published posts', async () => {
+    const getPosts = gql`
+        query {
+            posts {
+                id
+                title
+                body
+                published
+            }
+        }
+    `
+
+    const response = await client.query({query: getPosts})
+    expect(response.data.posts.length).toBe(1)
+    expect(response.data.posts[0].published).toBe(true)
+
+})
